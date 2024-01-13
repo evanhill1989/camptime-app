d9ee9a21329e28b0e26cebffc07a967c1f22eb74
EvanReact <evanhill1989@gmail.com>
2024-01-13 09:07:37 -0500

    Gestures

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { ScrollView, Text } from "react-native";
 import { Card } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
 
 const ContactScreen = () => {
   return (
     <ScrollView>
-      <Card wrapperStyle={{ margin: 20 }}>
-        <Card.Title>Contact Information</Card.Title>
-        <Card.Divider />
-        <Text>1 Camptime Way</Text>
-        <Text>Seattle, WA 98241</Text>
-        <Text style={{ marginBottom: 10 }}>U.S.A</Text>
-        <Text>Phone:1-855-242-2267</Text>
-        <Text>Email: campsites@camptime.com</Text>
-      </Card>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+        <Card wrapperStyle={{ margin: 20 }}>
+          <Card.Title>Contact Information</Card.Title>
+          <Card.Divider />
+          <Text>1 Camptime Way</Text>
+          <Text>Seattle, WA 98241</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A</Text>
+          <Text>Phone:1-855-242-2267</Text>
+          <Text>Email: campsites@camptime.com</Text>
+        </Card>
+      </Animatable.View>
     </ScrollView>
   );
 };
